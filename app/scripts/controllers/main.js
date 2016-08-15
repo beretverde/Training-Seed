ec6853ff890c6489435e306c8ce7cac30c4d656a
beretverde <tylerjacox@gmail.com>
2016-08-15 13:00:04 -0600

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trainingSeedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trainingSeedApp
+ */
+angular.module('trainingSeedApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
