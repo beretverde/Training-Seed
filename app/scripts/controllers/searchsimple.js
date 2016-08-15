e2a93505fe0dcf6a4fbd82a89825b566416e5d8b
beretverde <tylerjacox@gmail.com>
2016-08-15 14:01:12 -0600

    Availability Search simple

--- app/scripts/controllers/searchsimple.js
+++ app/scripts/controllers/searchsimple.js
@@ -8,10 +8,55 @@
  * Controller of the trainingSeedApp
  */
 angular.module('trainingSeedApp')
-  .controller('SearchsimpleCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SearchsimpleCtrl', function ($scope,$http) {
+
+  	$scope.origin='slc';
+    $scope.destination='den';
+    $scope.beginDate='2016-08-15T03:35:21.258Z';
+    $scope.endDate='2016-08-20T03:35:21.258Z';
+    $scope.type='ADT';
+    $scope.count=1;
+    $scope.currencyCode='USD';
+  
+
+    var data = $.param({
+            	origin: $scope.origin,
+                destination: $scope.destination,
+                beginDate:$scope.beginDate,
+                endDate:$scope.endDate,
+                passengers:[
+                {
+                	type:$scope.type,
+                	discountCode:$scope.discountCode,
+                	count:$scope.count
+                }
+                ],
+                promotionCode:$scope.promotionCode,
+                currencyCode:$scope.currencyCode,
+                loyaltyFilter:$scope.loyaltyFilter
+            });
+
+
+
+    var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            };
+
+
+	$http.post('http://demo.navitaire.com/dotrez/api/nsk/availability/search/simple', data, config)
+            .success(function (data, status, headers, config) {
+                $scope.PostDataResponse = data;
+            }) 
+
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            });
+
+
+
   });
